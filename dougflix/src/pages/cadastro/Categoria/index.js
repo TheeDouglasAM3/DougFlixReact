7895b673ec1df061ff8db41da2e36c56bc3a63ad
Douglas Marcelino <douglas_champion@hotmail.com>
2020-07-29 18:41:31 -0300

    Atualiza Categoria

--- dougflix/src/pages/cadastro/Categoria/index.js
+++ dougflix/src/pages/cadastro/Categoria/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroCategoria (){
+function CadastroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
 
+            <form>
+                <label>
+                    Nome da Categoria:
+                    <input type="text" />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
             <Link to="/">
-                Ir para a home
+                Ir para home
             </Link>
         </PageDefault>
     )
