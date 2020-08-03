3839e6724c9ea3cbb98ae5a423d96c77332d277a
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-02 22:52:13 -0300

    Atualiza cadastro de categorias exibindo-as

--- dougflix/src/pages/cadastro/Categoria/index.js
+++ dougflix/src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import PageDefault from '../../../components/PageDefault'
 import FormField from '../../../components/FormField'
@@ -26,6 +26,20 @@ function CadastroCategoria() {
       event.target.value,
     )
   }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/'
+    fetch(`${URL}categorias`)
+      .then(async (response) => {
+        const responseJSON = await response.json()
+        setCategorias([
+          ...responseJSON,
+        ])
+      })
+  }, [
+    valoresForm.nome,
+  ])
+
   // mudar cor do form style={{background: valoresForm.cor}}
   return (
     <PageDefault>
@@ -75,6 +89,11 @@ function CadastroCategoria() {
         </Button>
       </form>
 
+      { categorias.length === 0 && (
+      <div>
+        Loading...
+      </div>
+      )}
       <ul>
         {categorias.map((categoria) => (
           <li key={`${categoria.nome}`}>
