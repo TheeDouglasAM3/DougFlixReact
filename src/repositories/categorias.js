0774ddac1609b6916bad126d6db68e5ae52a3caa
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-07 23:34:44 -0300

    Atualiza repositório de categorias (getAll)

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,18 @@ import config from '../config'
 
 const URL_CATEGORIES = `${config.URL_BACKEND}/categorias`
 
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (response) => {
+      if (response.ok) {
+        const responseJson = await response.json()
+        return responseJson
+      }
+
+      throw new Error('Não foi possível pegar os dados :(( ')
+    })
+}
+
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (response) => {
@@ -15,5 +27,6 @@ function getAllWithVideos() {
 }
 
 export default {
+  getAll,
   getAllWithVideos,
 }
