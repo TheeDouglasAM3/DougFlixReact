fbf3979e66a7d03ed827589282d650e41ca10bac
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-14 18:03:35 -0300

    Update page cadastro/categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import categoriasRepository from '../../../repositories/categorias'
 import PageDefault from '../../../components/PageDefault'
 import FormField from '../../../components/FormField'
 import Button from '../../../components/Button'
 import useForm from '../../../hooks/useForm'
+
+import './styles.css'
 
 function CadastroCategoria() {
   const [categorias, setCategorias] = useState([])
   const valoresIniciaisForm = {
     titulo: '',
     descricao: '',
-    cor: '#ffffff',
+    cor: '#DDDDDD',
   }
 
   const { handleChange, valoresForm, clearForm } = useForm(valoresIniciaisForm)
+  const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/'
+    : 'https://dougflixreact.herokuapp.com/'
 
   useEffect(() => {
-    const URL = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8080/'
-      : 'https://dougflixreact.herokuapp.com/'
     fetch(`${URL}categorias`)
       .then(async (response) => {
         const responseJSON = await response.json()
@@ -30,6 +33,22 @@ function CadastroCategoria() {
     valoresForm.titulo,
   ])
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    setCategorias([
+      ...categorias, valoresForm,
+    ])
+
+    categoriasRepository.store(valoresForm)
+
+    clearForm()
+  }
+
+  function deleteCategory(event, id) {
+    categoriasRepository.drop(id)
+    event.target.parentNode.parentNode.classList.add('hide')
+  }
+
   // mudar cor do form style={{background: valoresForm.cor}}
   return (
     <PageDefault menuPathRoute="/cadastro/video" menuNameButton="Novo vídeo">
@@ -38,14 +57,7 @@ function CadastroCategoria() {
         {valoresForm.titulo}
       </h1>
 
-      <form onSubmit={function handleSubmit(event) {
-        event.preventDefault()
-        setCategorias([
-          ...categorias, valoresForm,
-        ])
-        clearForm()
-      }}
-      >
+      <form onSubmit={handleSubmit}>
 
         <FormField
           label="Nome da Categoria"
@@ -84,10 +96,16 @@ function CadastroCategoria() {
         Loading...
       </div>
       )}
+      <br />
       <ul>
         {categorias.map((categoria) => (
           <li key={`${categoria.titulo}`}>
-            {categoria.titulo}
+            <p>
+              {categoria.titulo}
+            </p>
+            <div>
+              <i className="fas fa-trash-alt" onClick={(event) => deleteCategory(event, categoria.id)} aria-hidden="true" />
+            </div>
           </li>
         ))}
       </ul>
