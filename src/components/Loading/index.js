1bacb0acb492980d7625d3e211bff021f96c3541
Douglas Marcelino <douglas_champion@hotmail.com>
2020-08-15 17:56:44 -0300

    Create Loading animated component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Grid from 'react-spinners-css/dist/Grid'
+import './styles.css'
+
+const Loading = () => (
+  <div className="loading-area">
+    <Grid color="var(--primary)" />
+  </div>
+)
+
+export default Loading
